dbbe5e1987f41b18b1fc38237dd964c6ec192018
Tyler Green <tgreen8091@gmail.com>
2016-06-08 04:50:04 +0000

    Initial commit, including tests which start the server and db migration

---
+++ spec/e2e/apiSpec.js
@@ -0,0 +1,77 @@
+'use strict';
+
+var request = require('request');
+var dbSession = require('../../src/backend/dbSession.js');
+var Server = require('../../src/backend/server.js').Server;
+var resetDatabase = require('../resetDatabase.js');
+var async = require('async');
+
+describe('The API', function () {
+
+  var server;
+  
+  beforeEach(function (done) {
+    server = Server('8081');
+    server.listen(function (err) {
+      resetDatabase(dbSession, function () {
+        done(err);
+      });
+    });
+  });
+
+  afterEach(function (done) {
+    server.close(function() {
+      resetDatabase(dbSession, function() {
+        done();
+      });
+    });
+  });
+
+  it('should respond to a GET request at /api/keywords/', function (done) {
+    var expected = {
+      "_items": [
+        {'id': 1, 'value': 'Aubergine', 'categoryID': 1},
+        {'id': 2, 'value': 'Onion', 'categoryID': 1},
+        {'id': 3, 'value': 'Knife', 'categoryID': 2},
+      ]
+    };
+
+    async.series(
+      [
+        function(callback) {
+          resetDatabase(dbSession, callback);
+        },
+        function(callback) {
+          dbSession.insert(
+            'keyword',
+            { 'value':'Aubergine','categoryID': 1},
+            function(err) { callback(err) });
+        },
+        function(callback) {
+          dbSession.insert(
+            'keyword',
+            { 'value':'Onion','categoryID': 1},
+            function(err) { callback(err) });
+        },
+        function(callback) {
+          dbSession.insert(
+            'keyword',
+            { 'value':'Knife','categoryID': 2},
+            function(err) { callback(err) });
+        }
+      ],
+    function(err, results) {
+      if (err) throw err;
+      request.get(
+        {
+          'url': 'http://localhost:8081/api/keywords/',
+          'json': true
+        },
+      function (err, res, body) {
+        expect(res.statusCode).toBe(200);
+        expect(body).toEqual(expected);
+        done();
+      });
+    });
+  });
+});
